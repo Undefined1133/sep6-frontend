1ebfaf387264ed6386ac8b5fe174ac3911120a57
Undefined1133 <coolnick0111@gmail.com>
2023-05-31 18:54:29 +0200

    Removed unnecessary workflow, which was a demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,6 @@ export default function Home() {
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
-          <Link href="/login">
-              Login
-          </Link>
-          <Link href="/register">
-              Register
-          </Link>
-          <Link href="/movies">
-              Movies
-          </Link>
       </div>
     </main>
   )
